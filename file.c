56097b6a92601baeabba87dad64b2b543252d04c
Gabriel Proença Alves <gproenca42@gmail.com>
2023-08-28 18:28:48 -0300

    Add file_load/unload functions

--- file.c
+++ file.c
@@ -1,8 +1,34 @@
 #include "file.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-struct book_database {
+struct book_database
+{
   FILE* file;
   int size;
   int book_ptr;
 };
+
+library* file_load(const char* file_path)
+{
+  library* lib = malloc(sizeof(library));
+  lib->file = fopen(file_path, "rb+");
+
+  if(lib->file == NULL)
+    lib->file = fopen(file_path, "wb+");
+
+  if (fread(&lib->size, sizeof(int), 1, lib->file) == 0) {
+    int zero = 0;
+    fwrite(&zero, sizeof(int), 1, lib->file);
+  }
+
+  lib->book_ptr = 1;
+
+  return lib;
+}
+
+void file_unload(library* lib)
+{
+  fclose(lib->file);
+  free(lib);
+}
--- file.h
+++ file.h
@@ -1,1 +1,7 @@
+#include <stdio.h>
+#ifndef FILE_H
+#define FILE_H
+
 typedef struct book_database library;
+
+#endif
