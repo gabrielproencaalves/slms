06bca2b6750acdeee8721903bd76e181f20eb894
Gabriel Proença Alves <gproenca42@gmail.com>
2023-09-15 17:20:25 -0300

    Add DEFAULT_PROMPT_MSG and DBPATH macros

--- config.h
+++ config.h
@@ -1,3 +1,7 @@
 #define MAXWIDTH     80 /* Default vt* width:  80  chars  */
 #define MAXHEIGHT    24 /* Default vt* height: 24  chars  */
 #define TABWIDTH      2 /* Default tab width:   4  spaces */
+
+#define DEFAULT_PROMPT_MSG "> "
+
+#define DBPATH "default.csv"
