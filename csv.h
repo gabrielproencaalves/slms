cea46f6f79fe2dce90500fafbe79150ac467f78d
Gabriel Proença Alves <gproenca42@gmail.com>
2023-09-17 20:04:56 -0300

    Add csv library header

---
+++ csv.h
@@ -0,0 +1,13 @@
+#ifndef CSV
+#define CSV
+
+#define MAX_HEADERS_NUM 7
+
+typedef struct csv_file {
+  FILE* file;
+  char* headers[MAX_HEADERS_NUM];
+  int n_headers;
+  unsigned long int line;
+} CSV;
+
+#endif
