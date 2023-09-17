e6d66b04c2ba1e6fb88803c94534b37e3cf4d60f
Gabriel Proença Alves <gproenca42@gmail.com>
2023-09-17 19:54:15 -0300

    Remove MAXPROMPTMSG and prompt_msg

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #define NCMD 6          /* Number of available commands */
 #define MAXCMDLEN 6     /* Length of largest command */
-#define MAXPROMPTMSG 32 /* Maximum length of prompt message */
 
 enum CMD {
   ADD,
@@ -15,8 +14,6 @@ enum CMD {
 };
 
 extern char BUFFER[];
-
-char prompt_msg[MAXPROMPTMSG] = "> ";
 
 static char* CMDS[] = {
   "add",
@@ -31,38 +28,34 @@ int main(void)
 {
   initialize();
 
-  while (prompt(0, prompt_msg) != EOF) {
+  while (prompt(0, DEFAULT_PROMPT_MSG) != EOF) {
     int CMD_INDEX;
 
     for (CMD_INDEX = 0; CMD_INDEX < NCMD; ++CMD_INDEX) {
-      if (strncmp(BUFFER, CMDS[CMD_INDEX], MAXCMDLEN) == 0)
+      if (strncmp(BUFFER, CMDS[CMD_INDEX], MAXCMDLEN) == 0) {
         switch (CMD_INDEX) {
           case ADD:
-            /* Add book*/
-            prints("AB");
             break;
           case REMOVE:
-            /* Remove book */
-            prints("RB");
+
             break;
           case MODIFY:
-            /* Modify book */
-            prints("MB");
+
             break;
           case SEARCH:
-            /* Search book */
-            prints("SB");
+
             break;
           case BORROW:
-            /* Borrow book */
-            prints("BB");
+
             break;
           case QUIT:
-            prints("Quitting...");
-            /* Quit */
             return 0;
         }
+
+        break;
+      }
     }
   }
+
   return 0;
 }
