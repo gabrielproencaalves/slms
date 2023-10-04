ff9a7ce9bb49f4443bce33de4eb46d056ff352a6
Gabriel Proença Alves <gproenca42@gmail.com>
2023-10-04 18:20:59 -0300

    Gnulize main.c

--- main.c
+++ main.c
@@ -5,7 +5,8 @@
 #define NCMD 6          /* Number of available commands */
 #define MAXCMDLEN 6     /* Length of largest command */
 
-enum CMD {
+enum CMD
+{
   ADD,
   REMOVE,
   MODIFY,
@@ -16,7 +17,8 @@ enum CMD {
 
 extern char BUFFER[];
 
-static char* CMDS[] = {
+static char* CMDS[] =
+{
   "add",
   "remove",
   "modify",
@@ -25,42 +27,47 @@ static char* CMDS[] = {
   "quit",
 };
 
-int main(void)
+int
+main(void)
 {
   initialize();
   CSV* db = csv_open(DBPATH);
 
-  while (prompt(0, DEFAULT_PROMPT_MSG) != EOF) {
-    int CMD_INDEX;
+  while (prompt(0, DEFAULT_PROMPT_MSG) != EOF)
+    {
+      int CMD_INDEX;
 
-    for (CMD_INDEX = 0; CMD_INDEX < NCMD; ++CMD_INDEX) {
-      if (strncmp(BUFFER, CMDS[CMD_INDEX], MAXCMDLEN) == 0) {
-        switch (CMD_INDEX) {
-          case ADD:
-            add_book(db);
-            break;
-          case REMOVE:
+      for (CMD_INDEX = 0; CMD_INDEX < NCMD; ++CMD_INDEX)
+        {
+          if (strncmp(BUFFER, CMDS[CMD_INDEX], MAXCMDLEN) == 0)
+            {
+              switch (CMD_INDEX)
+                {
+                  case ADD:
+                    add_book(db);
+                    break;
+                  case REMOVE:
 
-            break;
-          case MODIFY:
+                    break;
+                  case MODIFY:
 
-            break;
-          case SEARCH:
+                    break;
+                  case SEARCH:
 
-            break;
-          case BORROW:
+                    break;
+                  case BORROW:
 
-            break;
-          case QUIT:
-            csv_close(db);
-            return 0;
-            break;
+                    break;
+                  case QUIT:
+                    csv_close(db);
+                    return 0;
+                    break;
+                }
+
+              break;
+            }
         }
-
-        break;
-      }
     }
-  }
 
   csv_close(db);
   return 0;
