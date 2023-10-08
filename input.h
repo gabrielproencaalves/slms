276c56400bf0f4e6a1a61808c2007ebb77fbc9d6
Gabriel Proença Alves <gproenca42@gmail.com>
2023-10-08 17:26:09 -0300

    Add input functions protoypes to input.h

---
+++ input.h
@@ -0,0 +1,10 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+/* readline: reads n characters from stdin */
+int readline(int);
+
+/* prompt: prints s and reads n characters from stdin */
+int prompt(int, const char*);
+
+#endif /* INPUT_H */
